8abfedbf23fe8c8e25ead9b0291e3d6ce756e24e
alexbiviano <bivianoalex63@gmail.com>
2025-05-20 15:35:23 +0200

    [upd] mostrate copertine in pagina

--- react-boolflix/src/App.jsx
+++ react-boolflix/src/App.jsx
@@ -3,12 +3,15 @@ import axios from 'axios'
 import ReactCountryFlag from 'react-country-flag'
 
 const languageToCountryCode = {  
-  en: 'GB',
-  'en-US': 'US', 
+  en: 'GB',  
+  'en-US': 'US',  
   fr: 'FR',  
   it: 'IT',  
-  es: 'ES', 
+  es: 'ES',  
+  de: 'DE',  
 };  
+
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w342'; 
 
 function App() {  
   const [query, setQuery] = useState('');  
@@ -21,17 +24,16 @@ function App() {
       const moviesResponse = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}&language=it_IT`);  
       const tvResponse = await axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${query}&language=it_IT`);  
       
-    
       const combinedResults = [...moviesResponse.data.results, ...tvResponse.data.results];  
       
-     
       const normalizedResults = combinedResults.map(item => ({  
         id: item.id,  
-        title: item.title || item.name, 
-        original_title: item.original_title || item.original_name, 
+        title: item.title || item.name,  
+        original_title: item.original_title || item.original_name,  
         original_language: item.original_language,  
         vote_average: item.vote_average,  
-        media_type: item.media_type || (item.title ? 'movie' : 'tv') 
+        media_type: item.media_type || (item.title ? 'movie' : 'tv'),  
+        poster_path: item.poster_path 
       }));  
 
       setResults(normalizedResults);  
@@ -42,7 +44,7 @@ function App() {
 
   return (  
     <div className="container">  
-      <h1 className="my-4">Cerca Film e Serie TV</h1>   
+      <h1 className="my-4">Cerca Film e Serie TV</h1>  
       <div className="input-group mb-3">  
         <input   
           type="text"   
@@ -59,9 +61,12 @@ function App() {
       <div className="row">  
         {results.map((item) => {  
           const countryCode = languageToCountryCode[item.original_language] || item.original_language.toUpperCase();  
+          const posterUrl = item.poster_path ? `${IMAGE_BASE_URL}${item.poster_path}` : 'path/to/default/image.png';  
+
           return (  
             <div className="col-md-4 mb-4" key={item.id}>  
               <div className="card">  
+                <img src={posterUrl} className="card-img-top" alt={item.title} /> 
                 <div className="card-body">  
                   <h5 className="card-title">{item.title}</h5>  
                   <h6 className="card-subtitle mb-2 text-muted">{item.original_title}</h6>  
@@ -86,7 +91,7 @@ function App() {
                     )}  
                   </p>  
                   <p className="card-text">Voto: {item.vote_average}</p>  
-                  <p className="card-text">Tipo: {item.media_type}</p>
+                  <p className="card-text">Tipo: {item.media_type}</p>  
                 </div>  
               </div>  
             </div>  
